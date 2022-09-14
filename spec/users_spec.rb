9ca6b98a41534830cf2c8e25f5818eb897c30f03
fridayshoes <one_amp@hotmail.com>
2022-09-14 21:30:38 +0100

    created test for authenticating users

--- spec/users_spec.rb
+++ spec/users_spec.rb
@@ -9,6 +9,26 @@ RSpec.describe User do
       expect(user.name).to eq 'admin'
     end
   end
+
+  describe '.authenticate method' do
+    it 'returns a user and logs them in if correct email and password are given' do
+      user = User.create(name: 'admin', username: 'admin', email: 'admin@makersacademy.com', password: 'password123')
+      authenticated_user = User.authenticate(username: 'admin', password: 'password123')
+      expect(authenticated_user.user_id).to eq user.user_id
+    end
+
+    it "returns nil if incorrect username given" do
+      user = User.create(name: 'admin', username: 'admin', email: 'admin@makersacademy.com', password: 'password123')
+      expect(User.authenticate(username: 'bob', password: 'password123')).to be_nil
+    end
+
+    it "returns nil if incorrect password given" do
+      user = User.create(name: 'admin', username: 'admin', email: 'admin@makersacademy.com', password: 'password123')
+      expect(User.authenticate(username: 'admin', password: 'password')).to be_nil
+    end
+  end
+    
+
 end
 
     
