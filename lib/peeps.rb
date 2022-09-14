2994aed98bd951bff3ddda381013f84251d3beae
fridayshoes <one_amp@hotmail.com>
2022-09-14 13:54:15 +0100

    test and method created for create peeps

--- lib/peeps.rb
+++ lib/peeps.rb
@@ -4,11 +4,24 @@ class Peeps
 
   attr_reader :id, :peep, :created_on
   
-  def initialize(id:, peep:, created_on:)
-    @id  = id
+  def initialize(peep:, created_on:)
+    # @id  = id
     @peep = peep
     @created_on = created_on
   end
+
+  def self.create(peep:, created_on:)
+    
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'chitter_test')
+    else
+      connection = PG.connect(dbname: 'chitter')
+    end
+
+    result = connection.exec_params("INSERT INTO peeps (peep, created_on) VALUES($1, $2)RETURNING peep, created_on;", [peep, created_on])
+    Peeps.new(peep: result[0]["peep"], created_on: result[0]["created_on"])
+  end
+
 
   def self.all
     # [
@@ -17,12 +30,17 @@ class Peeps
     #   "Why is he sniffing so much?",
     #   "I am in the curry house",
     # ]
-    connection = PG.connect(dbname: 'chitter')
+    # connection = PG.connect(dbname: 'chitter')
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'chitter_test')
+    else
+      connection = PG.connect(dbname: 'chitter')
+    end
     result = connection.exec("SELECT * FROM peeps ORDER BY id DESC;") #Ask DB to sort list descending by id
     # result.map { |message| message['peep'] }
     result.map do |message| 
       # message['peep']
-      Peeps.new(id: message['id'], peep: message['peep'], created_on: message['created_on'])
+      Peeps.new(peep: message['peep'], created_on: message['created_on'])
     end
   end
 end
--- spec/peeps_spec.rb
+++ spec/peeps_spec.rb
@@ -1,8 +1,22 @@
 require 'peeps'
 
-describe Peeps do
-  describe '.all' do
+RSpec.describe Peeps do
+  describe '.create method' do
+    it 'creates a new peep' do
+      message = Peeps.create(peep: 'hello everyone', created_on: '2022-06-10 21:56:16.759063')
+
+      expect(message.peep).to eq 'hello everyone'
+    end
+  end
+
+  describe '.all method' do
     it 'returns all peeps' do
+      
+      Peeps.create(peep: 'hello everyone', created_on: '2022-06-10 21:56:16.759063')
+      Peeps.create(peep: 'Still got this bloomin lurgy and high temperature.', created_on: '2022-06-12 18:41:54.015632')
+      Peeps.create(peep: 'Why is he sniffing so much?',	created_on: '2022-06-12 18:46:32.850734')
+      Peeps.create(peep: 'I am in the curry house',	created_on: '2022-06-12 18:47:03.577594')
+
       peeps = Peeps.all
 
       expect(peeps.last.peep).to eq "hello everyone"
